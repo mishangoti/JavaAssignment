a4c29e8028d5e48b8be23ea66c0a61639d58712b
mishangoti <mishan2512@gmail.com>
2023-04-13 14:01:38 -0400

    ex-6

--- src/com/assignment/task6/Ex_6.java
+++ src/com/assignment/task6/Ex_6.java
@@ -1,12 +1,45 @@
 package com.assignment.task6;
 
 class Complex {
-	int sum;
-	int difference;
+	double real;
+	double imaginary;
 	
-	public Complex (){
-		System.out.println("This is cons");
+	public Complex() {
+		this.real = 0;
+        this.imaginary = 0;
 	}
+	
+	public Complex (double real, double imaginary) {
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+	
+	public Complex(double num) {
+        this.real = num;
+        this.imaginary = num;
+    }
+	
+	public Complex add(Complex c2) {
+		double sumReal = this.real + c2.real;
+		double sumImaginary = this.imaginary + c2.imaginary;
+		return new Complex(sumReal, sumImaginary);
+	}
+	
+	public Complex substract(Complex c2) {
+		double diffReal = this.real - c2.real;
+		double diffImaginary = this.imaginary - c2.imaginary;
+		return new Complex(diffReal, diffImaginary);
+	}
+	
+	public Complex multiply(Complex c2) {
+		double productReal = this.real * c2.real;
+		double productImaginary = this.imaginary * c2.imaginary;
+		return new Complex(productReal, productImaginary);
+	}
+	
+	public void display() {
+        System.out.println(real + " + " + imaginary + "i");
+    }
 }
 
 public class Ex_6 {
@@ -14,8 +47,18 @@ public class Ex_6 {
 	public static void main(String[] args) {
 		// 6. Print the sum, difference, and product of two complex numbers by creating a class named 'Complex' with
 		// separate methods for each operation whose real and imaginary parts are entered by the user.
+		Complex c1 = Complex(2.5, 5.5);
+		Complex c2 = Complex(3.5, 4.5);
 		
+		Complex sum = c1.add(c2); 
+		Complex difference = c1.substract(c2);
+		Complex multiply = c1.multiply(c2);
 		
+		System.out.println("Sum: ");
+        sum.display();
+        System.out.println("Difference: ");
+        difference.display();
+        System.out.println("Product: ");
+        multiply.display();
 	}
-
 }
